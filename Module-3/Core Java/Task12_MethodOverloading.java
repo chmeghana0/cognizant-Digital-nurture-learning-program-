2e624ef2f1cc61e8433586c3d2cdd5c25a5fd8cd
Meghana <chmegu25@gmail.com>
2025-05-27 00:08:11 +0530

    Create Task12_MethodOverloading.java

---
+++ Module-3/Core Java/Task12_MethodOverloading.java
@@ -0,0 +1,19 @@
+public class MethodOverloading {
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static double add(double a, double b) {
+        return a + b;
+    }
+
+    static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Sum (int, int): " + add(5, 10));
+        System.out.println("Sum (double, double): " + add(3.5, 2.5));
+        System.out.println("Sum (int, int, int): " + add(1, 2, 3));
+    }
+}
