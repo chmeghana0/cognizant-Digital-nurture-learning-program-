540ae7a2c82f4cb4623ff6905f514d508c5097ba
Meghana <chmegu25@gmail.com>
2025-05-27 13:08:20 +0530

    Create Task14_ jqueryDemo.js

---
+++ Module_1/JavaScript/Task14_ jqueryDemo.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $("#registerBtn").click(function () {
+    alert("Registered via jQuery");
+  });
+
+  $(".event-card").fadeIn();
+
+  // Benefit of using frameworks like React/Vue:
+  console.log("Component-based structure and reactive UI");
+});
