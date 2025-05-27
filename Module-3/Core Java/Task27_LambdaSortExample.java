b2d0e1ae31b885c78f1f5cbdd4fd9517d777c9bd
Meghana <chmegu25@gmail.com>
2025-05-27 12:18:03 +0530

    Create Task27_LambdaSortExample.java

---
+++ Module-3/Core Java/Task27_LambdaSortExample.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class LambdaSortExample {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Zara", "John", "Bob", "Alice");
+        Collections.sort(names, (s1, s2) -> s1.compareTo(s2));
+        System.out.println("Sorted names: " + names);
+    }
+}
